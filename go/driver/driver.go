cb05693c2891baf889e97b54845b06e1a5d50670
Volodymyr Manilo <35466116+vmanilo@users.noreply.github.com>
2018-07-03 13:21:36 +0300

    Print out decoded values in stdout

--- go/driver/driver.go
+++ go/driver/driver.go
@@ -2,10 +2,12 @@ package driver
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // DecodedPayload represents a decoded payload
@@ -51,32 +53,58 @@ func (d *Driver) Run(args []string) (success bool) {
 
 func (d *Driver) run(args []string) error {
 	if len(args) < 1 {
-		return errors.New("subcommand required")
+		return errors.New(`subcommand required.
+	Supported commands: decode`)
 	}
 
 	cmd := args[0]
+	cmdArgs := args[1:]
 	switch cmd {
 	case "decode":
-		// TODO: decode the payload
-		bytes, err := hex.DecodeString(args[1])
-		if err != nil {
-			return errors.New("payload argument is not in hex")
-		}
+		return d.decode(cmdArgs)
 
-		port, err := strconv.ParseInt(args[2], 10, 32)
-		if err != nil {
-			return errors.New("port argument is not a string")
-		}
-
-		_, err = d.Decoder(bytes, int(port))
-		if err != nil {
-			return err
-		}
-
-		// TODO: print out the decoded payload to stdout
-
-		return nil
 	default:
 		return errors.New("invalid subcommand")
 	}
+}
+
+func (d *Driver) decode(args []string) error {
+	payload, port, err := parseDecodeArgs(args)
+	if err != nil {
+		return err
+	}
+
+	decoded, err := d.Decoder(payload, port)
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(decoded)
+	if err != nil {
+		return errors.New("failed to marshal in json decoded data")
+	}
+
+	fmt.Println(string(data))
+	return nil
+}
+
+func parseDecodeArgs(args []string) (payload []byte, port int, err error) {
+	if len(args) != 2 {
+		err = errors.New(`decode expected 2 arguments: hex payload and integer port
+	Usage: <driver binary> decode 110A000F00551001 1`)
+		return
+	}
+
+	payload, err = hex.DecodeString(strings.TrimLeft(args[0], "0x"))
+	if err != nil {
+		err = errors.New("payload argument is not in hex")
+		return
+	}
+
+	val, err := strconv.ParseInt(args[1], 10, 32)
+	if err != nil {
+		err = errors.New("port argument is not an integer")
+	}
+	port = int(val)
+	return
 }
